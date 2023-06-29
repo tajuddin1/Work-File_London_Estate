c99f0cd19034e13da57a97fd6095e91f774c080a
taj1uddin <itstaj7272@gmail.com>
2023-06-29 18:41:09 +0600

    Working On

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -14,3 +14,22 @@ numbers.forEach((num) => {
   };
   incrementCounter();
 });
+
+
+var navIcon = document.getElementById("navIcon");
+var leftmenu =document.getElementById("leftMenu");
+var body = document.getElementsByTagName("BODY")[0];
+
+document.onclick= function(e){
+    if(e.target.id !=='leftmenu' && e.target.id !=='navIcon'){
+        navIcon.classList.remove('active');
+        leftmenu.classList.remove('fitMenu');
+    }
+}
+
+navIcon.addEventListener('click' , function(){
+    navIcon.classList.toggle('active');
+    leftmenu.classList.toggle('fitMenu');
+    body.classList.toggle('overflow');
+     
+ });
