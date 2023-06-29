ff20c29f839840c750821994b77ad9415c489f62
taj1uddin <itstaj7272@gmail.com>
2023-06-30 03:50:24 +0600

    Edits

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,14 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-86px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
 const numbers = document.querySelectorAll(".number");
 
 numbers.forEach((num) => {
